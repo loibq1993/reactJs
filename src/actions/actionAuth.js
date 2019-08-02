08d496274c0b2d4166a3ee7fd3e03aacfaf064f1
loibq1993 <buiquangloi1993@gmail.com>
2019-08-03 01:10:56 +0700

    convert to async await

--- src/actions/actionAuth.js
+++ src/actions/actionAuth.js
@@ -2,15 +2,15 @@ import callApi from "../callApi";
 import * as act from "./actionCreators";
 
 export const actRequestLogin = (formData) => {
-    return (dispatch) => {
-        return callApi('auth/login','POST',formData)
-            .then((res) => {
-                console.log(res.data.access_token);
-                localStorage.setItem("access_token", res.data.access_token);
-                dispatch(act.actLogin(res.data));
-            })
-            .catch( (errors) => {
-                dispatch(act.actLoginFailed(errors.data))
-            });
+    return async (dispatch) => {
+        try {
+            const res = await callApi('auth/login', 'POST', formData);
+            console.log(res.data.access_token);
+            localStorage.setItem("access_token", res.data.access_token);
+            dispatch(act.actLogin(res.data));
+        }
+        catch (errors) {
+            dispatch(act.actLoginFailed(errors.data));
+        }
     }
 };
